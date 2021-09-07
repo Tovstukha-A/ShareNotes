c3fe792e910779e0362172afb7c2b77d3b3d971f
Andrii <tovstukha.au@gmail.com>
2021-09-07 22:59:27 +0300

    added css to the project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,26 @@
 function Main() {
     return (
-        <div>
+        <div className="container">
+            <div className="col-12">
+                <div className="text">
+                    <ul className="row button-list">
+                        <li className="col-6"><a href="/create" type=" button" className="btn btn-primary">Создать заметку</a></li>
+                        <li className="col-6"><a href="/note" type=" button" className="btn btn-primary">Посмотреть заметку</a></li>
+                    </ul>
+                </div>
 
+                <div className="text">
+                    <p><b>ShareNotes</b> – сервис для обмена заметками. Создайте заметку, затем отправьте ссылку на заметку и ваш друг сможет ее просмотреть.
+                        После просмотра заметка будет удалена (или по истечении 15 минут с момента создания).</p>
+                    <p>Как сделать заметку? </p>
+                    <ul>
+                        <li>Пройдите по ссылке.</li>
+                        <li>Вставьте или напишите текст и нажмите "Вперед".</li>
+                        <li>Отправьте сгенерированный адрес другу!</li>
+                    </ul>
+                    <p>Как прочитать заметку? Перейдите по присланному URL, либо введите адрес руками здесь.</p>
+                </div>
+            </div>
         </div>
     );
 }
